941c699b3149b28bda83a2372c5a08ff361acc2a
Shachar Racheli <r0583237001@gmail.com>
2024-11-19 19:22:09 +0200

    lesson6

---
+++ src/app/components/information-popup/information-popup.component.ts
@@ -0,0 +1,16 @@
+import { Component, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-information-popup',
+  standalone: true,
+  imports: [],
+  templateUrl: './information-popup.component.html',
+  styleUrl: './information-popup.component.css'
+})
+export class InformationPopupComponent {
+text:string='';
+showPopup(txt:string){
+  this.text= txt;
+}
+
+}
